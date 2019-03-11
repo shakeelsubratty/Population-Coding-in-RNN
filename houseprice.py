ad156cb2eafc4065276cf0389ddc7c1719254eab
shakeelsubratty <shakeel.subratty@gmail.com>
2019-03-11 13:37:08 +0000

    Rescaled, functioning housing predicion output

--- houseprice.py
+++ houseprice.py
@@ -4,6 +4,7 @@ import pop_coding
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn import preprocessing
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
@@ -12,12 +13,15 @@ from sklearn.metrics import accuracy_score
 
 # load dataset
 dataframe = pandas.read_csv("housing.csv", delim_whitespace=True, header=None)
-coded_dataframe = pop_coding.code_dataframe(dataframe)
-dataset = coded_dataframe.values
+dataset = dataframe.values
+data_scaler = pop_coding.code_dataframe(dataframe)
+coded_dataframe = data_scaler[0]
+min_max_scaler = data_scaler[1]
+coded_dataset = coded_dataframe.values
 
 # split into input (X) and output (Y)
-X = dataset[:, 0:130]
-Y = dataset[:, 130:140]
+X = coded_dataset[:, 0:130]
+Y = coded_dataset[:, 130:140]
 
 
 # define base model
@@ -36,7 +40,7 @@ numpy.random.seed(seed)
 # evaluate model with standardized dataset
 # estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=5, verbose=0)
 
-estimator = KerasRegressor(build_fn=baseline_model, epochs=10, batch_size=5, verbose=1)
+estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=5, verbose=1)
 # estimators.append(('standardize', StandardScaler()))
 # estimators.append(('mlp', KerasRegressor(build_fn=baseline_model, epochs=10, batch_size=5, verbose=1)))
 # pipeline = Pipeline(estimators)
@@ -45,11 +49,29 @@ estimator = KerasRegressor(build_fn=baseline_model, epochs=10, batch_size=5, ver
 # results = cross_val_score(estimator, X, Y, cv=kfold)
 # print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
 
-print(len(X))
-print(len(Y))
+# print(len(X))
+# print(len(Y))
 
 estimator.fit(X, Y)
 prediction = estimator.predict(X)
-print(prediction)
+# print(prediction)
 
-numpy.savetxt("housing_prediction.csv", prediction, delimiter=",")
+decoded_prediction = numpy.array(pop_coding.decode_prediction(prediction))
+
+# min_max_scaler = preprocessing.MinMaxScaler()
+# print(decoded_prediction)
+# print(decoded_prediction.reshape(1, 1))
+a = numpy.zeros([506, 14])
+# numpy.append(a,decoded_prediction)
+# print(a.shape, decoded_prediction.shape)
+# print(a)
+#
+# a = numpy.append(a,decoded_prediction.reshape(-1,1))
+a[:,13] = decoded_prediction
+# print(a)
+rescaled_decoded_prediction = min_max_scaler.inverse_transform(a)[:,13]
+print(rescaled_decoded_prediction)
+
+numpy.savetxt("housing_prediction_coded.csv", prediction, delimiter=",")
+numpy.savetxt("housing_prediction_decoded.csv", decoded_prediction, delimiter=",")
+numpy.savetxt("housing_prediction_decoded_rescaled.csv", rescaled_decoded_prediction, delimiter=",")
--- pop_coding.py
+++ pop_coding.py
@@ -34,6 +34,8 @@ def code_dataframe(input_dataframe):
     dataset_scaled = min_max_scaler.fit_transform(input_dataset)
     dataframe = pandas.DataFrame(dataset_scaled, columns=input_dataframe.columns)
 
+    numpy.savetxt("housing_dataset_scaled.csv", dataframe, delimiter=",")
+
     # print(dataframe)
     #
     # print(pandas.DataFrame(min_max_scaler.inverse_transform(dataset_scaled), columns=input_dataframe.columns))
@@ -50,7 +52,7 @@ def code_dataframe(input_dataframe):
     number_of_neurons = 10
 
     s = numpy.linspace(0.0, 1.0, num=number_of_neurons)
-    sigma_main = 0.15
+    sigma_main = 0.2
 
     coded_datasets = {}
 
@@ -64,10 +66,11 @@ def code_dataframe(input_dataframe):
             coded_datasets[i].loc[j] = code_for_x
 
     # Coding Y
-    coded_datasets[len(coded_datasets)] = pandas.DataFrame(columns=list(range(13*number_of_neurons,14*number_of_neurons)))
+    coded_datasets[len(coded_datasets)] = pandas.DataFrame(
+        columns=list(range(13 * number_of_neurons, 14 * number_of_neurons)))
     for j in range(len(Y)):
-        code_for_y = code(Y[j],s,sigma_main)
-        coded_datasets[len(coded_datasets)-1].loc[j] = code_for_y
+        code_for_y = code(Y[j], s, sigma_main)
+        coded_datasets[len(coded_datasets) - 1].loc[j] = code_for_y
 
     final_dataframe = coded_datasets[0]
 
@@ -77,9 +80,22 @@ def code_dataframe(input_dataframe):
     print(final_dataframe)
     plot_dataset(final_dataframe, 0, True)
 
-    numpy.savetxt("housing_target_prices_coded.csv", coded_datasets[len(coded_datasets)-1], delimiter=",")
+    numpy.savetxt("housing_target_prices_coded.csv", coded_datasets[len(coded_datasets) - 1], delimiter=",")
 
-    return final_dataframe
+    return [final_dataframe, min_max_scaler]
+
+
+def decode_prediction(input_prediction):
+    number_of_neurons = 10
+    s = numpy.linspace(0.0, 1.0, num=number_of_neurons)
+    output = []
+
+    for i in range(len(input_prediction)):
+        decoded_value = decode(input_prediction[i], s)
+        # print(decoded_value)
+        output.append(decoded_value)
+        # print(output)
+    return output
 
 
 def plot_dataset(dataset, y, logy):
